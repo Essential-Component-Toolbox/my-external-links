03340ee158f08fd6707b0479fcd7282ceb5e31ea
zdodson21 <zdodson21@gmail.com>
2024-05-07 11:08:35 -0400

    added logos for discord, untested, added support for image and column types

---
+++ my-social-media.js
@@ -0,0 +1,103 @@
+import {LitElement, html, css} from 'lit';
+
+export class MySocialMedia extends LitElement {
+    
+    static get tag() {
+        return 'my-social-media';
+    }
+
+    constructor() {
+        super();
+        this.discordLink = '';
+        this.facebookLink = '';
+        this.githubLink = '';
+        this.instagramLink = '';
+        this.playstationLink = '';
+        this.snapchatLink = '';
+        this.twitterLink = '';
+        this.xLink = '';
+        this.xboxlink = '';
+        this.nintendoLink = '';
+        this.pinterestLink = '';
+
+
+        this.darkIcons = false;
+        this.type = 'logos';
+    }
+
+    static get styles() {
+        return css`
+            
+        `
+    }
+
+    render() {
+        // TODO make it so it goes to the proper file path for light-logos or dark-logos
+        let logoColor = '';
+        if (this.darkIcons === false) {
+            logoColor = 'light-logos';
+        } else {
+            logoColor = 'dark-logos';
+        }
+
+        return html`
+            <div class="my-social-media-container">
+                ${this.type === 'logos' ? html`
+                    <div class="type-logos">
+                        ${this.discordLink !== '' ? html`<a href="${this.discordLink}"><img class='logo' src="/assets/${logoColor}/discord.png" alt="Link to Discord"/></a>` : ''}
+                    </div>
+                ` : ''}
+
+                ${this.type === 'column' ? html`
+                    <div class="type-column">
+                        ${this.discordLink !== '' ? html`
+                            <div class='discord-row'>
+                                <a href="${this.discordLink}"><img class='logo' src="/assets/${logoColor}/discord.png" alt="Link to Discord"/></a>
+                                <a href="${this.discordLink}"><p>Discord</p></a>
+                            </div>
+                        ` : ''}
+                    </div>
+                ` : ''}
+            </div>
+        `
+    }
+
+    static get properties() {
+        return {
+            facebookLink: {
+                type: String,
+                attribute: 'facebook-link'
+            },
+            twitterLink: {
+                type: String
+            },
+            xLink: {
+                type: String
+            },
+            discordLink: {
+                type: String
+            },
+            githubLink: {
+                type: String
+            },
+            playstationLink: {
+                type: String
+            },
+            xboxlink: {
+                type: String
+            },
+            instagramLink: {
+                type: String
+            },
+            snapchatLink: {
+                type: String
+            },
+            darkIcons: {
+                type: Boolean
+            }
+        };
+    }
+}
+
+globalThis.customElements.define(MySocialMedia.tag, MySocialMedia);
+export {MySocialMedia};
